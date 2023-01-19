97eba17077764e6b76ced1cf15a2b57d2d458c2c
SoloMylan <mylankoobs@gmail.com>
2023-01-19 22:02:09 +0100

    2D klasse toegevoegd
    - 2d toegevoegd
    - functie om 1d buren te krijgen toegevoegd

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import random as rnd
 import pygame
 import time
 
+#Globale functies
+
+def get_neighbours1D(rooster, idx, reach):
+    states = []
+    for i in range(idx-reach, idx+reach+1):
+        if i > 0 and i < rooster.size:
+            states.append(rooster[i])
+        else:
+            states.append(0)
+    return states
+
 class CellularAutomata():
     
     def __init__(self, shape, rules):
@@ -21,7 +32,7 @@ class CellularAutomata():
         for idx, cell in np.ndenumerate(self.rooster):
             self.rooster[idx] = rnd.choice((0,1))
 
-class Cellullar1D(CellularAutomata):
+class Cellular1D(CellularAutomata):
 
     def __init__(self, size, rules):
         super().__init__((size,1), rules)
@@ -30,6 +41,9 @@ class Cellullar1D(CellularAutomata):
 
     def start_middle(self):
         self.rooster[(self.shape[0])//2] = 1
+    
+    
+                
 
 
     def draw(self, screen):
@@ -75,17 +89,63 @@ class Cellullar1D(CellularAutomata):
                     #changetime = 2000 #ms
                 last = pygame.time.get_ticks()
                 
-#class Cellular2D(CellularAutomata(shape, rules))
+class Cellular2D(CellularAutomata):
+    
+    def __init__(self, width, height, rules):
+        super().__init__((width, height), rules)
+        self.width = width
+        self.height = height
+        
+    def draw(self, screen):
+        self.cellsize = self.SCREEN_WIDTH//self.width
+        #print(cellsize)
+        screen.fill((0,0,0))
+        surf = pygame.Surface((self.cellsize,self.cellsize))
+        if self.rainbowmode:
+            surf.fill((rnd.randint(0,255),rnd.randint(0,255),rnd.randint(0,255)))
+        else:
+            surf.fill((255,255,255))
+        for idx, cell in np.ndenumerate(self.rooster):
+            if cell > 0:
+                screen.blit(surf, (idx[0]*self.cellsize, idx[1]*self.cellsize))
+        pygame.display.flip()
+    
+    def run(self, width, height, changetime, rainbowmode):
+        """start pygame visualisatie met bepaalde width en height, changetime geeft tijd tussen updates en rainbowmode is bool"""
+        pygame.init()
+        self.rainbowmode = rainbowmode
+        self.SCREEN_WIDTH = width
+        self.SCREEN_HEIGHT = height
+        size = [self.SCREEN_WIDTH,self.SCREEN_HEIGHT]
+        self.changetime = changetime #ms
+        screen = pygame.display.set_mode(size)
+        running = True
+        last = pygame.time.get_ticks()
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+            now = pygame.time.get_ticks()
+            if now - last > changetime:
+                self.draw(screen)
+                self.update()
+                
+                last = pygame.time.get_ticks()
         
           
 def rule22(cell, idx, rooster):
-    if idx[0] > 0:
-        left = rooster[idx[0]-1,idx[1]] > 0
-    else: left = False
-    if idx[0] < rooster.shape[0] - 1:
-        right = rooster[idx[0]+1,idx[1]] > 0
-    else: right = False
-    center = cell > 0
+    states = get_neighbours1D(rooster, idx[0], 1)
+    left = states[0]
+    center = states[1]
+    right = states[2]
+    #if idx[0] > 0:
+     #   left = rooster[idx[0]-1,idx[1]] > 0
+    #else: left = False
+    #if idx[0] < rooster.shape[0] - 1:
+    #    right = rooster[idx[0]+1,idx[1]] > 0
+    #else: right = False
+    #center = cell > 0
 
     if left and center and right:
         return 0
@@ -135,6 +195,6 @@ def rule54(cell, idx, rooster):
     return cell
     
 
-game = Cellullar1D(128, rule22)
+game = Cellular1D(64, rule22)
 game.start_middle()
-game.run(1280,640, 100, True)
+game.run(640,640, 500, False)
